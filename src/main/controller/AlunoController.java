d8c5865a02422cb258d3626b468331bd99225985
dougl <dougl@Douglas>
2020-11-23 22:02:11 -0300

    Metodo inserir no controller(nao finalizado)

--- src/main/controller/AlunoController.java
+++ src/main/controller/AlunoController.java
@@ -1,5 +1,16 @@
 package controller;
+import java.time.LocalDate;
+import model.vo.AlunoVO;
+import model.vo.TurmaVO;
 
-public class AlunoController {
-
+public class AlunoController{
+	
+	public static void inserir(int id_pessoa, String observacoes, LocalDate data_matricula, String matricula, TurmaVO id_turma){
+		AlunoVO a = new AlunoVO(id_pessoa, observacoes, data_matricula, matricula, id_turma);
+		try {
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
 }
