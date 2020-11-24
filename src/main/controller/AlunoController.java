e29add651c3d81c8b7c247ebc0d13ef10b2b7855
dougl <dougl@Douglas>
2020-11-23 22:25:03 -0300

    criacao inserir PessoaController

--- src/main/controller/AlunoController.java
+++ src/main/controller/AlunoController.java
@@ -6,11 +6,14 @@ import model.vo.TurmaVO;
 public class AlunoController{
 	
 	public static void inserir(int id_pessoa, String observacoes, LocalDate data_matricula, String matricula, TurmaVO id_turma){
-		AlunoVO a = new AlunoVO(id_pessoa, observacoes, data_matricula, matricula, id_turma);
+		AlunoVO aluno = new AlunoVO(id_pessoa, observacoes, data_matricula, matricula, id_turma);
 		try {
 			
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
 	}
+	
+
+	
 }
--- src/main/controller/PessoaController.java
+++ src/main/controller/PessoaController.java
@@ -1,5 +1,16 @@
 package controller;
 
+import java.time.LocalDate;
+import model.vo.PessoaVO;
+
 public class PessoaController {
 
+	public static void inserir(int id, String nome, String cpf, LocalDate nascimento, char sexo, String telefone, String celular,
+			String email, String bairro, String cidade, char estado, String cep) {
+		
+		PessoaVO pessoa = new PessoaVO(id,nome,cpf,nascimento, sexo, telefone, celular, email, bairro, cidade, estado, cep);
+	}
 }
+
+
+
