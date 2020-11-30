3b7e95742a2ec384dc6584135e6af3f490ea0bfe
fabio <fabio@DESKTOP-B2BDTL0>
2020-11-30 20:35:27 -0300

    ajustes

--- src/main/view/TelaCheckIn.java
+++ src/main/view/TelaCheckIn.java
@@ -1,31 +1,59 @@
 package view;
 
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 
-public class TelaCheckIn extends JPanel {
-	private JTextField textField;
+public class TelaCheckIn extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtCpf;
 
 	/**
-	 * Create the panel.
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaCheckIn frame = new TelaCheckIn();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
 	 */
 	public TelaCheckIn() {
-		setLayout(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("CPF:");
-		lblNewLabel.setBounds(104, 118, 46, 14);
-		add(lblNewLabel);
+		JLabel lblNewLabel = new JLabel("CPF: ");
+		lblNewLabel.setBounds(63, 90, 46, 14);
+		contentPane.add(lblNewLabel);
 		
-		textField = new JTextField();
-		textField.setBounds(169, 115, 120, 20);
-		add(textField);
-		textField.setColumns(10);
+		txtCpf = new JTextField();
+		txtCpf.setBounds(133, 87, 132, 20);
+		contentPane.add(txtCpf);
+		txtCpf.setColumns(10);
 		
-		JButton btnNewButton = new JButton("check-in");
-		btnNewButton.setBounds(169, 245, 89, 23);
-		add(btnNewButton);
-
+		JButton btnCheckIn = new JButton("Check-In");
+		btnCheckIn.setBounds(149, 210, 89, 23);
+		contentPane.add(btnCheckIn);
 	}
+
 }
