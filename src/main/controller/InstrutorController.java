1c841480b50e963f3bc3e0f66609e3e02497297c
dougl <dougl@Douglas>
2020-11-30 20:37:48 -0300

    inicio da classe instrutor controller(inacabada)

--- src/main/controller/InstrutorController.java
+++ src/main/controller/InstrutorController.java
@@ -1,5 +1,23 @@
 package controller;
 
-public class InstrutorController {
+import model.bo.AlunoBO;
+import model.bo.InstrutorBO;
+import model.dao.ModalidadeDAO;
+import model.dao.TurmaDAO;
+import model.vo.AlunoVO;
 
+	public class InstrutorController extends PessoaController{
+		InstrutorBO bo = new InstrutorBO();
+		ModalidadeDAO modalidadeDAO = new ModalidadeDAO();
+		TurmaDAO turmaDAO = new TurmaDAO();
+
+		public String inserir(AlunoVO aluno) throws Exception {
+			try {
+				validarCampos(aluno);
+				
+				return bo.inserir(aluno);
+			} catch(Exception exception) {
+				return exception.getMessage();
+			}
+		}
 }
