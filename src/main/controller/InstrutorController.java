cdbcade54c114b6a23bdf92f6087cab8464665d6
dougl <dougl@Douglas>
2020-11-30 22:17:03 -0300

    correcao de erros

--- src/main/controller/InstrutorController.java
+++ src/main/controller/InstrutorController.java
@@ -6,50 +6,50 @@ import model.dao.TurmaDAO;
 import model.vo.AlunoVO;
 import model.vo.InstrutorVO;
 
-	public class InstrutorController extends PessoaController{
-		InstrutorBO bo = new InstrutorBO();
-		ModalidadeDAO modalidadeDAO = new ModalidadeDAO();
-		TurmaDAO turmaDAO = new TurmaDAO();
+public class InstrutorController extends PessoaController{
+	InstrutorBO bo = new InstrutorBO();
+	ModalidadeDAO modalidadeDAO = new ModalidadeDAO();
+	TurmaDAO turmaDAO = new TurmaDAO();
 
-		public String inserir(InstrutorVO instrutor) throws Exception {
-			try {			
-				validarCampos(instrutor);
-				return bo.inserir(instrutor);
-			} catch(Exception exception) {
-				return exception.getMessage();
-			}
+	public String inserir(InstrutorVO instrutor) throws Exception {
+		try {			
+			validarCampos(instrutor);
+			return bo.inserir(instrutor);
+		} catch(Exception exception) {
+			return exception.getMessage();
 		}
-		public String excluir(String instrutorId, String pesssoaId) throws Exception {
-			int instrutor 		= 0;
-			int pessoa 		= 0;
+	}
+	public String excluir(String instrutorId, String pesssoaId) throws Exception {
+		int instrutor 		= 0;
+		int pessoa 		= 0;
 
-			try {
-				instrutor 			= Integer.parseInt(instrutorId);
-				pessoa 			= Integer.parseInt(pesssoaId);
-				
-				return bo.excluir(instrutor, pessoa);
-			} catch (NumberFormatException e) {
-				return "Insira um número inteiro";
-			}	
-		}
-		
-		public String alterar(InstrutorVO instrutor) {
-			try {
-				validarCampos(instrutor);
-				
-				return bo.alterar(instrutor);
-			} catch (Exception exception) {
-				return exception.getMessage();
-			}
-		}
+		try {
+			instrutor 			= Integer.parseInt(instrutorId);
+			pessoa 			= Integer.parseInt(pesssoaId);
 
-		private static void validarCampos(InstrutorVO instrutor) throws Exception {
-			try {
-				if (instrutor.getNome().isEmpty() && (instrutor.getNome().length() > 100 || instrutor.getNome().length() < 3)) {
-					throw new Exception("Nome não pode ser vazio e deve conter entre 3 e 100 caracteres");
-				}
-			} catch (Exception exception) {
-				throw new Exception(exception.getMessage());
-			}
+			return bo.excluir(instrutor, pessoa);
+		} catch (NumberFormatException e) {
+			return "Insira um número inteiro";
+		}	
+	}
+
+	public String alterar(InstrutorVO instrutor) {
+		try {
+			validarCampos(instrutor);
+
+			return bo.alterar(instrutor);
+		} catch (Exception exception) {
+			return exception.getMessage();
 		}
+	}
+
+	private static void validarCampos(InstrutorVO instrutor) throws Exception {
+		try {
+			if (instrutor.getNome().isEmpty() && (instrutor.getNome().length() > 100 || instrutor.getNome().length() < 3)) {
+				throw new Exception("Nome não pode ser vazio e deve conter entre 3 e 100 caracteres");
+			}
+		} catch (Exception exception) {
+			throw new Exception(exception.getMessage());
+		}
+	}
 }
